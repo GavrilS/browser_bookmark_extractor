552449e2cd3bdec8ea785e1f9a7201fec94be4e9
GavrilS <gavr1lll@abv.bg>
2024-03-23 12:37:52 +0200

    Added  extractBookmarks function to make a list of all available bookmarks in the browser

--- popup.js
+++ popup.js
@@ -1,17 +1,43 @@
-const bookmarks = chrome.bookmarks.getTree(
+const bookmarkDict = {};
+const bookmarks = [];
+
+chrome.bookmarks.getTree(
     (results) => {
         console.log("Bookmarks:", results);
-        // results.children.forEach((result) => {
-        //     console.log(result)
-        // });
+        results.forEach((bookmark) => {
+            // console.log(bookmark);
+            extractBookmarks(bookmark);
+        });
+        console.log('Bookmarks final: ', bookmarks);
     }
 );
+
+function extractBookmarks(bookmark) {
+    if (bookmark.children) {
+        bookmark.children.forEach((bookmarkChild) => {
+            extractBookmarks(bookmarkChild);
+        });
+    } else {
+        let parent = '';
+        if (bookmark.parentId) {
+            parent = bookmark.parentId;
+        }
+        bookmrk = {
+            'parent': parent,
+            'id': bookmark.id,
+            'title': bookmark.title,
+            'url': bookmark.url ? bookmark.url : ''
+        };
+        bookmarks.push(bookmrk);
+    }
+}
 
 const extractBtn = document.getElementById('extract-full-bookmarks');
 const importBtn = document.getElementById('import-bookmarks');
 
 extractBtn.addEventListener('click', () => {
-    console.log('Clicked extractBtn!');
+    // console.log('Clicked extractBtn!');
+
 });
 
 importBtn.addEventListener('click', () => {
