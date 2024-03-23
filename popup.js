66c1f245c409530f834c77ee91ce636b5afea45d
GavrilS <gavr1lll@abv.bg>
2024-03-23 21:13:43 +0200

    Added functionality to extract button to save bookmarks to a file, but need to make it wait for the result of the query to extract bookmarks

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 const bookmarkDict = {};
-const bookmarks = [];
+let bookmarks = [];
 
-chrome.bookmarks.getTree(
-    (results) => {
-        console.log("Bookmarks:", results);
-        results.forEach((bookmark) => {
-            // console.log(bookmark);
-            extractBookmarks(bookmark);
-        });
-        console.log('Bookmarks final: ', bookmarks);
-    }
-);
+// chrome.bookmarks.getTree(
+//     (results) => {
+//         console.log("Bookmarks:", results);
+//         results.forEach((bookmark) => {
+//             // console.log(bookmark);
+//             extractBookmarks(bookmark);
+//         });
+//         console.log('Bookmarks final: ', bookmarks);
+//     }
+// );
 
 function extractBookmarks(bookmark) {
     if (bookmark.children) {
@@ -36,9 +36,37 @@ const extractBtn = document.getElementById('extract-full-bookmarks');
 const importBtn = document.getElementById('import-bookmarks');
 
 extractBtn.addEventListener('click', () => {
-    // console.log('Clicked extractBtn!');
+    if (bookmarks.length > 0) {
+        bookmarks = [];
+    }
+    chrome.bookmarks.getTree(
+        (results) => {
+            console.log("Bookmarks:", results);
+            results.forEach((bookmark) => {
+                // console.log(bookmark);
+                extractBookmarks(bookmark);
+            });
+            console.log('Bookmarks final: ', bookmarks);
+        }
+    );
 
+    console.log(bookmarks.length);
+    saveBookmarks(bookmarks);
 });
+
+function saveBookmarks(bookmarkList) {
+    // const blob = new Blob([bookmarkList.toString()], {
+    //     type: 'text/plain;charset=utf-8',
+    // });
+    // saveAs(blob, 'bookmarks.txt');
+    const link = document.createElement('a');
+    const content = bookmarkList.toString();
+    const file = new Blob([content], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = 'bookmarks.txt';
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
 
 importBtn.addEventListener('click', () => {
     console.log('Clicked importBtn!');
